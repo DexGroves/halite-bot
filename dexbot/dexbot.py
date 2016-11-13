b60d263f19df6b78e37e0a498849f36ee790e4cf
Dex Groves <Dex.Groves@gmail.com>
2016-11-13 22:45:36 +0000

    Upgrade bot to be smart enough to win, but too slow

--- dexbot/dexbot.py
+++ dexbot/dexbot.py
@@ -16,12 +16,12 @@ class DexBot(object):
         for d in CARDINALS:
             if self.can_capture(gameMap, location, d):
                 return Move(location, d)
-
+        # Need to move away from any existing 255's
         # Else, calculate the nearest front, and move towards it if
         # strength at edge > self.requisite_str
         nearest_brdr = self.sg.get_nearest_brdr(location, gameMap)
-        str_to_brdr = self.sg.strength_to_brdr(location, gameMap, nearest_brdr)
-        if str_to_brdr > self.requisite_str:
+        # str_to_brdr = self.sg.strength_to_brdr(location, gameMap, nearest_brdr)
+        if gameMap.getSite(location).strength > self.requisite_str:
             new_site = gameMap.getSite(location, nearest_brdr)
             if new_site.owner == self.id:
                 return Move(location, nearest_brdr)
@@ -34,6 +34,7 @@ class DexBot(object):
         new_site = gameMap.getSite(location, d)
         return new_site.owner != self.id and site.strength > new_site.strength
 
+
 class Spyglass(object):
     """Scours the map for information."""
 
@@ -44,27 +45,28 @@ class Spyglass(object):
         border_dists = self.distance_to_brdr(location, gameMap)
         return min(border_dists, key=border_dists.get)
 
-    def distance_to_brdr(self, location, gameMap):
+    def distance_to_brdr(self, start_loc, gameMap):
         distances = {}
         for d in CARDINALS:
             distance = 0
-            new_site = gameMap.getSite(location)
+            location = start_loc
+            new_site = gameMap.getSite(start_loc)
             while new_site.owner == self.id:
                 distance += 1
                 location = self.move_cursor(location, d, gameMap)
                 new_site = gameMap.getSite(location)
-                if distance > max(gameMap.width, gameMap.height):
+                if distance > gameMap.width:
                     break
             distances[d] = distance
         return distances
 
     def strength_to_brdr(self, location, gameMap, d):
-        strength = 0
         new_site = gameMap.getSite(location)
+        strength = new_site.strength
         while new_site.owner == self.id:
-            strength += new_site.strength
             location = self.move_cursor(location, d, gameMap)
             new_site = gameMap.getSite(location)
+            strength += new_site.strength
             if strength > 255:
                 break
         return strength
@@ -74,10 +76,10 @@ class Spyglass(object):
         max_x = gameMap.width
         max_y = gameMap.height
         if direction == NORTH:
-            return Location(location.x, (location.y + 1) % max_y)
+            return Location(location.x, (location.y - 1) % max_y)  #-????????
         if direction == EAST:
             return Location((location.x + 1) % max_x, location.y)
         if direction == SOUTH:
-            return Location(location.x, (location.y - 1) % max_y)
+            return Location(location.x, (location.y + 1) % max_y)  # +????
         if direction == WEST:
             return Location((location.x - 1) % max_x, location.y)
