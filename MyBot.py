1634daa3518d5706d78f3f36252dd6645475ccff
Dex Groves <Dex.Groves@gmail.com>
2016-11-19 23:18:41 +0000

    Scan over known points to quicken outer loop

--- MyBot.py
+++ MyBot.py
@@ -18,10 +18,10 @@ while True:
     mapeval.set_evaluation(game_map)
     db.set_evaluator(mapeval)
 
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            location = Location(x, y)
-            owner = game_map.getSite(location).owner
-            if owner == my_id:
-                moves.append(db.move(location, game_map))
+    self_pts = mapeval.get_self_pts()
+
+    for x, y in self_pts:
+        location = Location(x, y)
+        moves.append(db.move(location, game_map))
+
     sendFrame(moves)
