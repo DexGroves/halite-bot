e235bac90a3a708b05ee2fab27839d002448308a
Groves <Declan.Groves@allstate.com>
2016-11-24 17:06:52 -0600

    More tuning

--- multicore_playoff.R
+++ multicore_playoff.R
@@ -15,19 +15,16 @@ build_call <- function(dim, nopp, seed) {
   call
 }
 
-# Sys.setenv(PATH = paste(Sys.getenv("PATH"),
-#                         "/nas/isg_prodops_work/dgrov/projects/halsrc",
-#                          sep = ":"))
-
 # Run nrun randomly sampled configs against RefBot
-nrun <- 20
-ncore <- 20
-seeds <- seq(2000, 2000 + nrun)
+nrun <- 15
+ncore <- 74
+start_seed <- 3000
+seeds <- seq(start_seed, start_seed + nrun)
 
 registerDoMC(ncore)
 results <- foreach(seed = seeds, .combine = c) %dopar% {
   cat(".")
-  build_call(25, 1, seed) %>%
+  build_call(40, 3, seed) %>%
     system(intern = TRUE) %>%
     paste(collapse = "\n") %>%
     str_extract("(?<=DexBot, came in rank #)[0-9]")
@@ -45,3 +42,11 @@ table(results)
 # results just m1
 #   1   2
 #  70 131
+# results
+#   1   2   3   4   5
+# 529 502 395 278 297
+
+# HU conf
+# HU: 57-18
+# MW conf
+# HU: 47-28
