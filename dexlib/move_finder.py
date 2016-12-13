cb47ff4d59170a60e16c9f3030722c1d03da4f59
Dex Groves <Dex.Groves@gmail.com>
2016-12-13 23:24:34 +0000

    Rework move acquisition

--- dexlib/move_finder.py
+++ dexlib/move_finder.py
@@ -22,6 +22,8 @@ class MoveFinder:
         self.noncombat_wait = config['noncombat_wait']
         self.max_wait = config['max_wait']
 
+        self.globality = 1
+
         self.min_dpdt = config['min_dpdt']
         self.roi_skew = config['roi_skew']
 
@@ -29,7 +31,7 @@ class MoveFinder:
 
         self.global_exponent = config['global_exponent']
 
-        # self.locality_value = self.get_locality_value(ms)
+        self.locality_value = self.get_locality_value(ms)
 
     def update(self, ms):
         pass
@@ -51,8 +53,6 @@ class MoveFinder:
 
         active_set = np.zeros_like(ms.owned)
         active_gradient = np.zeros_like(ms.owned, dtype=float)
-        active_k = np.zeros_like(active_gradient)
-        active_ttc = np.zeros_like(active_gradient)
         assigned_str = np.zeros_like(active_gradient)
         assigned_prod = np.zeros_like(active_gradient)
         assigned_prod.fill(0.0001)
@@ -80,6 +80,7 @@ class MoveFinder:
 
             gradient *= utilisation  # Penalty for not being able to hit 0.1 util.
             gradient *= str_bonus    # Bonus for having leftover capacity
+            gradient += self.brdr_global
 
             tx, ty = np.unravel_index(gradient.argmax(), gradient.shape)
 
@@ -115,6 +116,7 @@ class MoveFinder:
 
             gradient *= utilisation  # Penalty for not being able to hit 0.1 util.
             gradient *= str_bonus    # Bonus for having leftover capacity
+            gradient += self.brdr_global
 
             # Gradient for active border squares
             new_t2c = np.divide(np.maximum(1, (ms.strn - assigned_str) - ms.strn[x, y]),
@@ -146,3 +148,37 @@ class MoveFinder:
         # Assign moves
         for k, v in moves.items():
             mr.add_move(v)
+
+    def set_base_locality(self, ms):
+        """Get the inherent value of squares on the map based on
+        their access to production.
+        """
+        locality_value = np.zeros((ms.width, ms.height))
+        for x in range(ms.width):
+            for y in range(ms.height):
+                map_value = np.divide(ms.prod, ms.str_to[x, y, :, :])
+                map_value[x, y] = 0
+                locality_value[x, y] = np.sum(map_value)
+
+        data_max = maximum_filter(locality_value, 4, mode="wrap")
+
+        self.maxima = (locality_value == data_max)
+        self.base_locality = locality_value
+
+    def set_locality(self, ms):
+        """Find the border squares with the best access to production maxima
+        and boost them.
+        """
+        self.brdr_global = np.zeros_like(ms.prod, dtype=float)
+
+        maxima = copy(self.maxima)
+        maxima[np.nonzero(ms.owned)] = False
+        maxima[np.nonzero(ms.enemy)] = False
+
+        # probably a better numpy way to do this
+        for mx, my in np.transpose(np.nonzero(maxima)):
+            dists = [ms.str_to[mx, my, bx, by] for (bx, by) in ms.border_locs]
+            bestx, besty = ms.border_locs[np.argmin(dists)]
+            self.brdr_global[bestx, besty] += \
+                self.base_locality[mx, my] * np.sqrt(ms.capacity) * \
+                self.globality / np.min(dists)
