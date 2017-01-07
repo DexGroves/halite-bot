df427bef6c90f31ab709e6f02a5e5350914be5f3
Dex Groves <Dex.Groves@gmail.com>
2017-01-07 14:41:22 +0000

    Move resolver into its own house

---
+++ dexlib/resolver.py
@@ -0,0 +1,133 @@
+import numpy as np
+from dexlib.nphlt import Move
+from dexlib.find_path import find_pref_next
+
+
+class Resolver:
+    """Handle str cap avoiding, patch mechanics, etc."""
+
+    def __init__(self, gm):
+        ox, oy = gm.owned_locs[0]
+        self.parity = (ox + oy + gm.turn) % 2
+
+    def resolve(self, moves, gm):
+        # I don't do anything about over-growing the cap, but can I even.
+        output = []
+
+        self.set_implicit_stays(moves, gm)
+        pstrn_map = np.zeros_like(gm.strn)
+
+        on_moves = {(ax, ay): v for (ax, ay), v in moves.items()
+                    if (ax + ay + gm.turn) % 2 == self.parity}
+        off_moves = {(ax, ay): v for (ax, ay), v in moves.items()
+                     if (ax + ay + gm.turn) % 2 != self.parity}
+
+        if gm.turn < 20:  # TEst hacks
+            on_moves = moves
+            off_moves = {}
+
+        # Handle all the black squares going where they need to be
+        on_origins = list(on_moves.keys())
+        on_targets = list(on_moves.values())
+        on_strns = [gm.strn[x, y] for (x, y) in on_origins]
+        str_sort = np.argsort(on_strns)
+
+        for i in str_sort[::-1]:
+            ax, ay = on_origins[i]
+            tx, ty = on_targets[i]
+            istrn = on_strns[i]
+
+            (px1, py1, d1), (px2, py2, d2) = find_pref_next(ax, ay, tx, ty, gm)
+            if (istrn + pstrn_map[px1, py1]) <= 255:
+                output.append(Move(ax, ay, d1))
+                pstrn_map[px1, py1] += istrn
+                # logging.debug(((ax, ay), 'to', (d1), 'firstpick'))
+            elif px2 is not None and (istrn + pstrn_map[px2, py2]) <= 255:
+                output.append(Move(ax, ay, d2))
+                pstrn_map[px2, py2] += istrn
+                # logging.debug(((ax, ay), 'to', (d2), 'secpick'))
+            else:
+                output.append(Move(ax, ay, 0))
+                pstrn_map[ax, ay] += istrn + gm.prod[ax, ay]
+                # logging.debug(((ax, ay), 'to', (0), 'dodgeroo'))
+
+        # Handle all the white squares getting the heck out of the way
+        # Not iterating in any particular order!
+        for (ax, ay) in off_moves.keys():
+            istrn = gm.strn[ax, ay]
+            iprod = gm.prod[ax, ay]
+            if pstrn_map[ax, ay] == 0:
+                output.append(Move(ax, ay, 0))
+                pstrn_map[ax, ay] += istrn + iprod
+
+            elif (pstrn_map[ax, ay] + istrn + iprod) <= 255:
+                output.append(Move(ax, ay, 0))
+                pstrn_map[ax, ay] += istrn + iprod
+
+            else:  # Dodge this!
+                # Check if it's better to just hang out
+                addable = 255 - pstrn_map[ax, ay] - istrn
+                if addable > istrn:
+                    output.append(Move(ax, ay, 0))
+                    pstrn_map[ax, ay] += istrn + iprod
+                    continue
+
+                nbrs = gm.nbrs[ax, ay]
+
+                # Find somewhere to fit!
+                can_fit = np.array([
+                    gm.owned[nx, ny] and (pstrn_map[nx, ny] + istrn) <= 255
+                    for (nx, ny) in nbrs
+                ])
+
+                if can_fit.max() > 0:
+                    dir_ = can_fit.argmax() + 1
+                    output.append(Move(ax, ay, dir_))
+                    nx, ny = nbrs[can_fit.argmax()]
+                    pstrn_map[nx, ny] += istrn
+                    continue
+
+                # Find an enemy to hit!
+                # Can technically lose to cap here since I skip checking pstrn
+                enemy_strn = np.array([
+                    gm.enemy[nx, ny] * gm.strn[nx, ny]
+                    for (nx, ny) in nbrs
+                ])
+                if enemy_strn.max() > 1:
+                    dir_ = enemy_strn.argmax() + 1
+                    output.append(Move(ax, ay, dir_))
+                    nx, ny = nbrs[enemy_strn.argmax()]
+                    pstrn_map[nx, ny] += istrn
+                    continue
+
+                # Find a blank square to damage!
+                blank_strn = np.array([
+                    gm.blank[nx, ny] * gm.strnc[nx, ny]
+                    for (nx, ny) in nbrs
+                ])
+
+                if blank_strn.max() > 0.5:
+                    dir_ = blank_strn.argmax() + 1
+                    output.append(Move(ax, ay, dir_))
+                    nx, ny = nbrs[blank_strn.argmax()]
+                    pstrn_map[nx, ny] += istrn
+                    continue
+
+                # Go to the weakest remaining square
+                owned_strn = np.array([
+                    gm.owned[nx, ny] * gm.strn[nx, ny]
+                    for (nx, ny) in nbrs
+                ])
+
+                dir_ = owned_strn.argmin() + 1
+                output.append(Move(ax, ay, dir_))
+                nx, ny = nbrs[owned_strn.argmax()]
+                pstrn_map[nx, ny] += istrn
+                continue
+
+        return output
+
+    def set_implicit_stays(self, moves, gm):
+        implicit_stays = set((x, y) for x, y in gm.owned_locs) - moves.keys()
+        for ix, iy in implicit_stays:
+            moves[(ix, iy)] = (ix, iy)
