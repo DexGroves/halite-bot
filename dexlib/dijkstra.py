7cdd92c739043eb037aa64073299c1f94058e4cb
Dex Groves <Dex.Groves@gmail.com>
2017-01-08 18:18:09 +0000

    Remove networkx import

--- dexlib/dijkstra.py
+++ dexlib/dijkstra.py
@@ -2,7 +2,6 @@
 
 
 import itertools
-import networkx as nex
 import numpy as np
 from scipy.sparse import dok_matrix
 from scipy.sparse.csgraph import dijkstra
@@ -52,55 +51,6 @@ class ShortestPather:
         neighbours = [((x + 1) % self.w, y), ((x - 1) % self.w, y),
                       (x, (y + 1) % self.h), (x, (y - 1) % self.h)]
         return neighbours
-
-    def get_vertex(self, x, y):
-        """Return the index of vertices containing the pt x, y."""
-        return (x * self.h) + y
-
-
-class InternalPather:
-
-    def __init__(self, ms):
-        self.w, self.h = ms.prod.shape
-        self.vertices = list(itertools.product(range(self.w), range(self.h)))
-        self.nbrs = {v: self.get_nbrs(v) for v in range(len(self.vertices))}
-
-        self.G = nex.Graph()
-        self.G.add_nodes_from(self.vertices)
-
-        self.cache = {}
-        # pickle.dump(file=open('argh.pk', 'wb'), obj=self.vertices)
-
-    def update(self, ms):
-        # Faster to do incremental but this probably isn't slow
-        self.G.remove_edges_from(self.G.edges())
-        self.cache = {}
-        for x, y in ms.owned_locs:
-            vi = self.get_vertex(x, y)
-            for ni in self.nbrs[vi]:
-                self.G.add_edge(vi, ni)
-
-    def get_path_and_len(self, x, y, tx, ty):
-        vo = self.get_vertex(x, y)
-        vd = self.get_vertex(tx, ty)
-
-        # logging.debug(('getpath', x, y, tx, ty, self.cache))
-        if (vo, vd) not in self.cache:
-            try:
-                sp = nex.shortest_path(self.G, source=vo, target=vd)
-                self.cache[(vo, vd)] = self.vertices[sp[1]], len(sp)
-            except:
-                self.cache[(vo, vd)] = (tx, ty), 99999
-            # logging.debug(sp)
-            # logging.debug([self.vertices[s] for s in sp])
-        return self.cache[(vo, vd)]
-
-    def get_nbrs(self, v):
-        """Return a dictionary of the neighbours and cost of x, y."""
-        x, y = self.vertices[v]
-        neighbours = [((x + 1) % self.w, y), ((x - 1) % self.w, y),
-                      (x, (y + 1) % self.h), (x, (y - 1) % self.h)]
-        return [self.get_vertex(nx, ny) for (nx, ny) in neighbours]
 
     def get_vertex(self, x, y):
         """Return the index of vertices containing the pt x, y."""
