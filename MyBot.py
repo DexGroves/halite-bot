ae82f2d86199ac6a8cafa58dc2c08e18a7ed8345
Dex Groves <Dex.Groves@gmail.com>
2017-02-08 23:33:56 +0000

    Update clasess

--- MyBot.py
+++ MyBot.py
@@ -8,22 +8,44 @@ from dexlib.resolver import Resolver
 logging.basicConfig(filename='wtf.info', level=logging.DEBUG, filemode="w")
 
 
+class Moveset:
+    """A mutable move manager!"""
+
+    def __init__(self, gm):
+        self.to_move = set(((x, y) for (x, y) in gm.owned_locs))
+        self.move_dict = {}
+        self.move_matrix = np.zeros_like(gm.owned, dtype=bool)
+
+    def add_move(self, ax, ay, tx, ty, dir_=None):
+        """Register a move."""
+        self.move_dict[(ax, ay)] = tx, ty, dir_
+        self.move_matrix[ax, ay] = True
+        if (ax, ay) in self.to_move:
+            self.to_move.remove((ax, ay))
+
+    def iter_remaining(self):
+        """Iterate over all remaining moves."""
+        for ax, ay in self.to_move:
+            yield ax, ay
+
+    def process_moves(self):
+        """Convert everything to what the hlt.py expects."""
+        return [hlt.Move(ax, ay, dir_)
+                for (ax, ay), (_, _, dir_) in self.move_dict.items()]
+
+
 class Combatant:
     """Handle all the moves for combat zones."""
 
     def __init__(self, combat_wait):
         self.combat_wait = combat_wait
 
-    def decide_combat_moves(self, gm):
-        self.moves = {}
-        self.moved = np.zeros_like(gm.owned)
+    def decide_combat_moves(self, gm, moveset):
+        self.decide_melee_moves(gm, moveset)
+        self.decide_close_moves(gm, moveset)
+        return moveset
 
-        self.decide_melee_moves(gm)
-        self.decide_close_moves(gm)
-
-        return self.moved
-
-    def decide_melee_moves(self, gm):
+    def decide_melee_moves(self, gm, moveset):
         locs = np.transpose(np.nonzero(gm.melee_mat))
         strns = [gm.strn[x, y] for (x, y) in locs]
 
@@ -32,41 +54,29 @@ class Combatant:
             if gm.strnc[cx, cy] < (gm.prodc[cx, cy] * self.combat_wait):
                 continue
 
-            # if (cx + cy + gm.turn % 2) != gm.parity:
-            #     self.moves[cx, cy] = cx, cy
-            #     self.moved[cx, cy] = True
-
             nbrs = gm.nbrs[(cx, cy)]
             scores = [gm.combat_heur[nx, ny] for (nx, ny) in nbrs]
 
             nx, ny = nbrs[np.argmax(scores)]
-            self.moves[(cx, cy)] = nx, ny
-            self.moved[cx, cy] = True
             gm.combat_heur[nx, ny] /= 10000
 
-            nx, ny = nbrs[np.argmax(scores)]
+            moveset.add_move(cx, cy, nx, ny)
 
-    def decide_close_moves(self, gm):
+    def decide_close_moves(self, gm, moveset):
         locs = np.transpose(np.nonzero(gm.close_to_combat))
         for cx, cy in locs:
             if gm.strnc[cx, cy] < (gm.prodc[cx, cy] * self.combat_wait):
                 continue
-
-            # if (cx + cy + gm.turn % 2) != gm.parity:
-            #     self.moves[cx, cy] = cx, cy
-            #     self.moved[cx, cy] = True
 
             dmat = np.divide(gm.melee_mat, gm.dists[cx, cy])
             tx, ty = np.unravel_index(dmat.argmax(), dmat.shape)
 
             if gm.dists[cx, cy, tx, ty] < 4 and \
                     gm.strnc[cx, cy] < (gm.prodc[cx, cy] * (self.combat_wait + 1.5)):
-                self.moves[(cx, cy)] = cx, cy
-                self.moved[cx, cy] = True
+                moveset.add_move(cx, cy, cx, cy)
 
             else:
-                self.moves[(cx, cy)] = tx, ty
-                self.moved[cx, cy] = True
+                moveset.add_move(cx, cy, tx, ty)
 
     def dump_moves(self, gm):
         return self.moves
@@ -82,14 +92,12 @@ class MoveMaker:
         self.bulk_mvmt_off = 10
         self.wait = wait
 
-    def decide_noncombat_moves(self, gm, moved):
+    def decide_noncombat_moves(self, gm, moveset):
         working_strn = gm.strn.copy()
-        self.to_travel = np.zeros_like(gm.prod)
-        self.moves = {}
 
         motile = ((gm.strnc >= gm.prodc * self.wait) * gm.owned).astype(bool)
         motile[np.nonzero(gm.gte_nbr)] = True
-        motile[np.nonzero(moved)] = False
+        motile[np.nonzero(moveset.move_matrix)] = False
         strn_avail = gm.ostrn * motile
 
         Vloc, Vmid, Vglob = self.get_cell_value(gm)
@@ -102,9 +110,7 @@ class MoveMaker:
         d1_conquered = np.ones_like(Vtot, dtype=bool)
         fully_alloc = np.zeros_like(Vtot, dtype=bool)
 
-        to_move = motile.copy()
-        to_move[np.nonzero(moved)] = False
-        to_move_locs = np.transpose(np.nonzero(to_move))
+        to_move_locs = np.transpose(np.nonzero(motile))
         to_move_strn = [gm.strn[x, y] for (x, y) in to_move_locs]
         for ai in np.argsort(to_move_strn)[::-1]:
             ax, ay = to_move_locs[ai]
@@ -113,14 +119,8 @@ class MoveMaker:
             prox_value = np.divide(Vmid, (gm.dists[ax, ay] + t2c)) * d1_conquered + \
                 np.divide(Vglob, (gm.dists[ax, ay] + self.bulk_mvmt_off))
             tx, ty = np.unravel_index(prox_value.argmax(), prox_value.shape)
-            self.moves[(ax, ay)] = tx, ty
-            self.to_travel[ax, ay] = gm.dists[ax, ay, tx, ty]
 
-            # if not fully_alloc[tx, ty] and gm.dists[ax, ay, tx, ty] <= 2:
-            #     working_strn[tx, ty] -= gm.strn[ax, ay]
-            #     if working_strn[tx, ty] < 0:
-            #         working_strn[tx, ty] = gm.strn[tx, ty]
-            #         fully_alloc[tx, ty] = True
+            moveset.add_move(ax, ay, tx, ty)
 
             # Add to a postprocessing queue for later
             if gm.dists[ax, ay, tx, ty] == 1:
@@ -130,10 +130,7 @@ class MoveMaker:
                     d1_conquered[tx, ty] = 0
 
         self.process_d1_teamups(gm)
-
-    def dump_moves(self, gm):
-        # Need to force corner moves, don't forget
-        return self.moves
+        return moveset
 
     def get_cell_value(self, gm):
         # local_value = gm.prodc * gm.ubrdr
@@ -164,39 +161,6 @@ class MoveMaker:
                     gm.strn[tx, ty] = 0
 
 
-def coalesce(moves, gm, bord_eval):
-    return moves
-    # newmoves = {}
-    # weakmoves = {(ax, ay): (tx, ty, dir_)
-    #              for (ax, ay), (tx, ty, dir_) in moves.items()
-    #              if gm.strn[ax, ay] < 95 and dir_ != 0 and
-    #                 bord_eval.to_travel[ax, ay] > 6}
-    # for (ax, ay), (tx, ty, dir_) in weakmoves.items():
-    #     if dir_ == 1 or dir_ == 3:
-    #         if (ax+2, ay) in weakmoves.keys() and weakmoves[(ax+2, ay)][2] == dir_:
-    #             newmoves[(ax, ay)] = ax+1, ay, 2
-    #             newmoves[(ax+2, ay)] = ax+1, ay, 4
-    #         elif (ax-2, ay) in weakmoves.keys() and weakmoves[(ax-2, ay)][2] == dir_:
-    #             newmoves[(ax, ay)] = ax-1, ay, 4
-    #             newmoves[(ax-2, ay)] = ax-1, ay, 2
-    #     if dir_ == 2 or dir_ == 4:
-    #         if (ax, ay+2) in weakmoves.keys() and weakmoves[(ax, ay+2)][2] == dir_:
-    #             newmoves[(ax, ay)] = ax, ay+1, 3
-    #             newmoves[(ax, ay+2)] = ax, ay+1, 1
-    #         elif (ax, ay-2) in weakmoves.keys() and weakmoves[(ax, ay-2)][2] == dir_:
-    #             newmoves[(ax, ay)] = ax, ay-1, 1
-    #             newmoves[(ax, ay-2)] = ax, ay-1, 3
-
-    # for k, v in newmoves.items():
-    #     moves[k] = v
-
-    # return moves
-
-
-def process_moves(moves):
-    return [hlt.Move(ax, ay, dir_) for (ax, ay), (_, _, dir_) in moves.items()]
-
-
 game_map = hlt.ImprovedGameMap(8)
 hlt.send_init("DexBotNeuer")
 game_map.get_frame()
@@ -212,18 +176,13 @@ while True:
     logging.debug('TURN ------------' + str(game_map.turn))
     game_map.update()
 
-    moved = combatant.decide_combat_moves(game_map)
-    bord_eval.decide_noncombat_moves(game_map, moved)
+    moveset = Moveset(game_map)
+    moveset = combatant.decide_combat_moves(game_map, moveset)
+    moveset = bord_eval.decide_noncombat_moves(game_map, moveset)
+    moveset = resolver.resolve(game_map, moveset)
 
-    comb_moves = combatant.dump_moves(game_map)
-    bord_moves = bord_eval.dump_moves(game_map)
-    resolved_moves = resolver.resolve({**comb_moves, **bord_moves}, game_map)
-    coalesce_moves = coalesce(resolved_moves, game_map, bord_eval)
-
-    hlt.send_frame(process_moves(coalesce_moves))
+    hlt.send_frame(moveset.process_moves())
     game_map.get_frame()
-
-
 
     # def decide_noncombat_moves(self, gm, moved):
     #     working_strn = gm.strn.copy()
